3b3a78427c369ba3814079151425fc6a549882bd
Sanket Upadhyay <sanket.upadhyay@infoud.co.in>
2017-08-20 18:02:05 +0530

    Updated start according to latest api changes

--- start.py
+++ start.py
@@ -24,26 +24,18 @@ def run(args):
     print("Initializing Cerebro ")
     cerebro = init()
     neurons = cerebro.manager.get_all()
-    print("Cerebro initialized with % neurons".format(len(neurons)))
-
-    print("Retrieving list of supported commands")
-    print(neurons)
-    for n in neurons.iterkeys():
-        print(n)
+    print("Cerebro initialized with {} neurons".format(len(neurons)))
 
     print("All system configured successfully.")
 
-    print("Initializing command")
     command = en.Command(args)
-    print("Command initialized")
 
-    print("Executing command")
     print("Command Executed with following result:")
     print(cerebro.process_command(command))
 
 
-print('Argument List:', str(sys.argv[1:]))
+print('Argument List: {}'.format(str(sys.argv[1:])))
 print("Running...")
-run(tuple(sys.argv[1:]))
+run(sys.argv[1:][0])
 
 
