127dbd85bbd967a174045a0c134c61f81bda6067
Sanket Upadhyay <sanket.upadhyay@infoud.co.in>
2017-08-20 23:16:46 +0530

    Core finder service refactored

---
+++ cerebro/tests/core/test_finder.py
@@ -0,0 +1,10 @@
+import unittest
+
+from cerebro.core import constants as const, config as cfg, finder as fn
+
+
+class TestNeuronsFinderService(unittest.TestCase):
+
+    def test_find_neurons(self):
+        cfg.add_neurons_location(const.STR_DEFAULT_NEURONS_PATH)
+        assert fn.find(cfg.get_neurons_path()) is not None
