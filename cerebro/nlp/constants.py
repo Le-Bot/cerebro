025e6dabcfa2974cd97c460fca3c4765ca917a23
Sanket Upadhyay <sanket.upadhyay@infoud.co.in>
2017-09-21 11:39:16 +0530

    Changed constants to use current directory path

--- cerebro/nlp/constants.py
+++ cerebro/nlp/constants.py
@@ -1,3 +1,6 @@
-DATA_SET_PATH = "./cerebro/data/datasets/dataset.csv"
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+DATA_SET_PATH = BASE_DIR + "/data/datasets/dataset.csv"
 COLUMN_FEATURE = "feature"
 COLUMN_LABEL = "label"
