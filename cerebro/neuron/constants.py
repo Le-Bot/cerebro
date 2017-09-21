df69b1ca3d7d126857aabcdeb3938e75b00d9b50
Sanket Upadhyay <sanket.upadhyay@infoud.co.in>
2017-09-21 11:39:03 +0530

    Changed constants to use current directory path

--- cerebro/neuron/constants.py
+++ cerebro/neuron/constants.py
@@ -1,4 +1,8 @@
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 STR_DEFAULT_RESPONSE = "Sorry, I could not process that."
 STR_KEYWORDS = "KEYWORDS"
 
-STR_DEFAULT_NEURONS_PATH = "./cerebro/data/neurons"
+
+STR_DEFAULT_NEURONS_PATH = BASE_DIR + "/data/neurons"
